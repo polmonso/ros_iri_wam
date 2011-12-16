3a941b33c128a5b653d9294698a7867cda26a485
sfoix <sfoix@224674b8-e365-4e73-a4a8-558dbbfec58c>
2011-12-16 19:57:06 +0000

    adding new iri_wam_controllers

---
+++ iri_wam_controllers/include/iri_wam_controller/iri_wam_controller.h
@@ -0,0 +1,31 @@
+#include <pr2_controller_interface/controller.h>
+#include <pr2_mechanism_model/joint.h>
+#include <ros/ros.h>
+#include <iri_wam_controller/SetAmplitude.h>
+#include <control_toolbox/pid.h>
+
+
+namespace iri_wam_controller_ns{
+
+class IriWamControllerClass: public pr2_controller_interface::Controller
+{
+private:
+  bool setAmplitude(iri_wam_controller::SetAmplitude::Request& req,
+                    iri_wam_controller::SetAmplitude::Response& resp);
+
+  pr2_mechanism_model::JointState* joint_state_;
+  double init_pos_;
+  double amplitude_;
+  ros::ServiceServer srv_;
+  control_toolbox::Pid pid_controller_;
+  pr2_mechanism_model::RobotState *robot_;
+  ros::Time time_of_last_cycle_;
+
+public:
+  virtual bool init(pr2_mechanism_model::RobotState *robot,
+                   ros::NodeHandle &n);
+  virtual void starting();
+  virtual void update();
+  virtual void stopping();
+};
+} 
---
+++ iri_wam_controllers/src/iri_wam_controller.cpp
@@ -0,0 +1,95 @@
+#include "iri_wam_controller/iri_wam_controller.h"
+#include <pluginlib/class_list_macros.h>
+
+namespace iri_wam_controller_ns {
+
+
+/// Controller initialization in non-realtime
+bool IriWamControllerClass::init(pr2_mechanism_model::RobotState *robot,
+                            ros::NodeHandle &n)
+{
+  std::string joint_name;
+  if (!n.getParam("joint_name", joint_name))
+  {
+    ROS_ERROR("No joint given in namespace: '%s')",
+              n.getNamespace().c_str());
+    return false;
+  }
+
+  joint_state_ = robot->getJointState(joint_name);
+  if (!joint_state_)
+  {
+    ROS_ERROR("IriWamController could not find joint named '%s'",
+              joint_name.c_str());
+    return false;
+  }
+
+  // advertise service to set amplitude
+  amplitude_ = 0.5;
+  srv_ = n.advertiseService("set_amplitude", 
+                            &IriWamControllerClass::setAmplitude, this);
+
+  // copy robot pointer so we can access time
+  robot_ = robot;
+
+  // construct pid controller
+  if (!pid_controller_.init(ros::NodeHandle(n, "pid_parameters"))){
+    ROS_ERROR("IriWamController could not construct PID controller for joint '%s'",
+              joint_name.c_str());
+    return false;
+  }
+
+  return true;
+}
+
+
+/// Controller startup in realtime
+void IriWamControllerClass::starting()
+{
+  init_pos_ = joint_state_->position_;
+  time_of_last_cycle_ = robot_->getTime();
+  pid_controller_.reset();
+}
+
+
+/// Controller update loop in realtime
+void IriWamControllerClass::update()
+{
+  double desired_pos = init_pos_ + amplitude_ * sin(ros::Time::now().toSec());
+  double current_pos = joint_state_->position_;
+  
+  ros::Duration dt = robot_->getTime() - time_of_last_cycle_;
+  time_of_last_cycle_ = robot_->getTime();
+
+  joint_state_->commanded_effort_ = pid_controller_.updatePid(current_pos-desired_pos, dt);
+}
+
+
+/// Controller stopping in realtime
+void IriWamControllerClass::stopping()
+{}
+
+/// Service call to set amplitude of sin
+bool IriWamControllerClass::setAmplitude(iri_wam_controller::SetAmplitude::Request& req,
+                                     iri_wam_controller::SetAmplitude::Response& resp)
+{
+  if (fabs(req.amplitude) < 2.0){
+    amplitude_ = req.amplitude;
+    ROS_INFO("IriWamcontroller: set amplitude too %f", req.amplitude);
+  }
+  else
+    ROS_WARN("IriWamController: requested amplitude %f too large", req.amplitude);
+
+  resp.amplitude = amplitude_;
+  return true;
+}
+
+
+
+} // namespace
+
+/// Register controller to pluginlib
+PLUGINLIB_DECLARE_CLASS(iri_wam_controller,IriWamController, 
+                         iri_wam_controller_ns::IriWamControllerClass, 
+                         pr2_controller_interface::Controller)
+
