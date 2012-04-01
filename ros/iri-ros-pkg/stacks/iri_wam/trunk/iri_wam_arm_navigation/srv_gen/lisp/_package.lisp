56c7bd956a91e086a882ef960ac5a76d43662c83
irojas <irojas@224674b8-e365-4e73-a4a8-558dbbfec58c>
2012-04-01 12:21:50 +0000

    Creacion de un servio local para pruebas co ompl_cartesian

---
+++ ros/iri-ros-pkg/stacks/iri_wam/trunk/iri_wam_arm_navigation/srv_gen/lisp/_package.lisp
@@ -0,0 +1,10 @@
+(cl:defpackage iri_wam_arm_navigation-srv
+  (:use )
+  (:export
+   "POSEPATH"
+   "<POSEPATH-REQUEST>"
+   "POSEPATH-REQUEST"
+   "<POSEPATH-RESPONSE>"
+   "POSEPATH-RESPONSE"
+  ))
+
---
+++ ros/iri-ros-pkg/stacks/iri_wam/trunk/iri_wam_arm_navigation/srv_gen/lisp/_package_PosePath.lisp
@@ -0,0 +1,6 @@
+(cl:in-package iri_wam_arm_navigation-srv)
+(cl:export '(POSE_STAMPED-VAL
+          POSE_STAMPED
+          POSE_STAMPED-VAL
+          POSE_STAMPED
+))
