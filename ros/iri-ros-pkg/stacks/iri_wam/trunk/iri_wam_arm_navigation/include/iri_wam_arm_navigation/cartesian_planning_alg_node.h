25961e3a96832c6782f3e05f518702bc05d5642b
irojas <irojas@224674b8-e365-4e73-a4a8-558dbbfec58c>
2012-04-27 12:37:19 +0000

    Finally resstruturing package,

--- ros/iri-ros-pkg/stacks/iri_wam/trunk/iri_wam_arm_navigation/include/iri_wam_arm_navigation/cartesian_planning_alg_node.h
+++ ros/iri-ros-pkg/stacks/iri_wam/trunk/iri_wam_arm_navigation/include/iri_wam_arm_navigation/cartesian_planning_alg_node.h
@@ -2,7 +2,8 @@
 #include <geometry_msgs/Pose.h>
 #include "iri_wam_arm_navigation/PosePath.h"
 #include "iri_wam_arm_navigation/cartesian_planning_alg.h"
-
+namespace cartesian_planning
+{
 class CartesianPlanAlgNode
 {
 
@@ -11,26 +12,31 @@ class CartesianPlanAlgNode
 		CartesianPlanAlgNode();
 
 		~CartesianPlanAlgNode();
-
+		void printNode(const int type,const ob::ScopedState<ompl::base::SE3StateSpace>& state)const;
 
 	private:
 	
 	
 	 void current_pos_CB(const geometry_msgs::Pose::ConstPtr& input);
-
-	
-	// void RosPoseOmplState()
-	
+     void RostoOmplState(const geometry_msgs::Pose& input,ob::ScopedState<ompl::base::SE3StateSpace> *ptr);
+	 bool getPath(iri_wam_arm_navigation::PosePath::Request &request, iri_wam_arm_navigation::PosePath::Response &response);
+	 void OmpltoRosMsg(og::PathGeometric* ptrPath, std::vector<geometry_msgs::Pose>& poses );
+	 
+	 //[Nodes]
 	 ros::NodeHandle root_handle_;
 	 ros::NodeHandle private_handle_;
 	 //[Subcriber]
 	 ros::Subscriber current_pos_sub;
-	 ros::Subscriber current_pos_stamped_sub;
+	 
 	 //[Publisher]
 	 ros::Publisher  diagnostics_pub;
 	 //[Service]
 	 ros::ServiceServer solution_service;
 	 
-	 CartesianPlanAlg *alg_;
-
+     int number_states;
+	 boost::mutex lock;
+	 CartesianPlanAlg alg_;
+	 ob::ScopedState<ompl::base::SE3StateSpace> *goal;
+	 ob::ScopedState<ompl::base::SE3StateSpace> *current;
 };
+}
