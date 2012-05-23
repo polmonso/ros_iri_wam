7938f644174214c1ffa23f258cd16450a3e3a96b
irojas <irojas@224674b8-e365-4e73-a4a8-558dbbfec58c>
2012-05-23 13:28:35 +0000

    commit files necessary to compilation

---
+++ iri_wam_arm_navigation/src/iri_wam_arm_navigation/msg/__init__.py
@@ -0,0 +1,7 @@
+from ._SimplePoseAction import *
+from ._SimplePoseFeedback import *
+from ._SimplePoseResult import *
+from ._SimplePoseGoal import *
+from ._SimplePoseActionGoal import *
+from ._SimplePoseActionResult import *
+from ._SimplePoseActionFeedback import *
