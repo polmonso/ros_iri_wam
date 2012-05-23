78596d2a229643632466125903eadb57d1f03ca7
irojas <irojas@224674b8-e365-4e73-a4a8-558dbbfec58c>
2012-05-23 13:27:47 +0000

    deleting unwanted directory

--- ros/iri-ros-pkg/stacks/iri_wam/trunk/iri_wam_arm_navigation/msg_gen/lisp/_package_SimplePoseGoal.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package iri_wam_arm_navigation-msg)
-(cl:export '(GOAL-VAL
-          GOAL
-          FRAME_ID-VAL
-          FRAME_ID
-))
--- ros/iri-ros-pkg/stacks/iri_wam/trunk/iri_wam_arm_navigation/msg_gen/lisp/_package_SimplePoseResult.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package iri_wam_arm_navigation-msg)
-(cl:export '(ERROR_CODE-VAL
-          ERROR_CODE
-))
