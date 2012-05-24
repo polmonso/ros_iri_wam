67e6d7c44a6848d9256870771ee2c346342dbc0c
sfoix <sfoix@224674b8-e365-4e73-a4a8-558dbbfec58c>
2012-05-24 14:38:41 +0000

    deleting unnecessary files. Please DO NOT upload UNNECESSARY FILES

--- ros/iri-ros-pkg/stacks/iri_wam/trunk/iri_wam_arm_navigation/src/iri_wam_arm_navigation/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._SimplePoseAction import *
-from ._SimplePoseFeedback import *
-from ._SimplePoseResult import *
-from ._SimplePoseGoal import *
-from ._SimplePoseActionGoal import *
-from ._SimplePoseActionResult import *
-from ._SimplePoseActionFeedback import *
