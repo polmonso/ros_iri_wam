9a5394583e39f7fd4c005ad634f5f86e5661d04d
irojas <irojas@224674b8-e365-4e73-a4a8-558dbbfec58c>
2012-06-06 11:38:42 +0000

    testing obstacles

--- iri_wam_arm_navigation/test/box.cpp
+++ iri_wam_arm_navigation/test/box.cpp
@@ -7,18 +7,65 @@ int main(int argc, char** argv) {
 
   ros::init(argc, argv, "vertical_stick");
 
-  ros::NodeHandle nh;
+  ros::NodeHandle nh("~");
+  ros::NodeHandle pub("/");
 
+  XmlRpc::XmlRpcValue val1;
+  double tx,ty,tz,qx,qy,qz,qw;
+  
+  nh.getParam("posicion_x",val1); 
+  tx=(double)val1;
+  ROS_WARN_STREAM("posicion_x: "<<tx); 
+  
+  nh.getParam("posicion_y",val1); 
+  ty=(double)val1;
+  ROS_WARN_STREAM("posicion_y: "<<ty); 
+  
+  nh.getParam("posicion_z",val1); 
+  tz=(double)val1;
+  ROS_WARN_STREAM("posicion_z: "<<tz); 
+  
+  nh.getParam("rotacion_x",val1); 
+  qx=(double)val1;
+  ROS_WARN_STREAM("rotacion_x: "<<qx); 
+  
+  nh.getParam("rotacion_y",val1); 
+  qy=(double)val1;
+  ROS_WARN_STREAM("rotacion_y: "<<qy); 
+  
+  nh.getParam("rotacion_z",val1); 
+  qz=(double)val1;
+  ROS_WARN_STREAM("rotacion_z: "<<qz); 
+  
+  nh.getParam("rotacion_w",val1); 
+  qw=(double)val1;
+  ROS_WARN_STREAM("rotacion_w: "<<qw); 
+
+  double ancho,alto,largo;
+  nh.getParam("width",val1); 
+  ancho=(double)val1;
+  ROS_WARN_STREAM("width: "<<ancho); 
+  nh.getParam("length",val1); 
+  largo=(double)val1;
+  ROS_WARN_STREAM("length: "<<largo); 
+  nh.getParam("height",val1); 
+  alto=(double)val1;
+  ROS_WARN_STREAM("height: "<<alto); 
+  std::string name_frame;
+  nh.getParam("name_obstacle",val1); 
+  name_frame=(std::string)val1;
+  ROS_WARN_STREAM("name_obstacle: "<<name_frame);   
+  
   ros::Publisher object_in_map_pub_;
-  object_in_map_pub_  = nh.advertise<arm_navigation_msgs::CollisionObject>("collision_object", 10);
+  object_in_map_pub_  = pub.advertise<arm_navigation_msgs::CollisionObject>("collision_object", 10);
 
-  sleep(2);
+
 
   //add the cylinder into the collision space
   arm_navigation_msgs::CollisionObject cylinder_object;
-  cylinder_object.id = "/boxs";
+  cylinder_object.id = "/"+name_frame;
   // COllision Operation
-  // ADD
+  // ADD 
   //REMOVE
   // DETACH_AND_ADD_AS_OBJECT
   //ATTACH_AND_REMOVE_AS_OBJECT
@@ -26,11 +73,6 @@ int main(int argc, char** argv) {
   cylinder_object.header.frame_id = "/world";
   cylinder_object.header.stamp = ros::Time::now();
   arm_navigation_msgs::Shape object;
-  //shape type
-  //CYLINDER
-  //SPHERE
-  //BOX
-  //MESH
   object.type = arm_navigation_msgs::Shape::BOX;
 
 // the origin of each shape is considered at the shape's center
@@ -44,42 +86,44 @@ int main(int argc, char** argv) {
 // the length is along the Z axis
 
 // for box
-// size_x := dimensions[0]
-// size_y := dimensions[1]
-// size_z := dimensions[2]
+// size_x := dimensions[0]//largo
+// size_y := dimensions[1]//ancho
+// size_z := dimensions[2]//alto
 
- /* object.dimensions.resize(2);
-  object.dimensions[0] = 0.02;
-  object.dimensions[1] = 1.5;
-  geometry_msgs::Pose pose;
-  pose.position.x = 0.5;
-  pose.position.y = 0.14;
-  pose.position.z = 0.75;
-  pose.orientation.x = 0;
-  pose.orientation.y = 0;
-  pose.orientation.z = 0;
-  pose.orientation.w = 1;*/
   object.dimensions.resize(3);
-  object.dimensions[0] = 0.25;
-  object.dimensions[1] = 0.25;
-  object.dimensions[2] = 0.5;
+  object.dimensions[0] = largo;
+  object.dimensions[1] = ancho;
+  object.dimensions[2] = alto;
+  
   geometry_msgs::Pose pose;
-  pose.position.x = 0.53;
-  pose.position.y = 0.14;
-  pose.position.z = 0.75;
-  pose.orientation.x = 0;
-  pose.orientation.y = 0;
-  pose.orientation.z = 0;
-  pose.orientation.w = 1;
+  pose.position.x = tx;
+  pose.position.y = ty;
+  pose.position.z = tz;
+  pose.orientation.x = qx;
+  pose.orientation.y = qy;
+  pose.orientation.z = qz;
+  pose.orientation.w = qw;
   cylinder_object.shapes.push_back(object);
   cylinder_object.poses.push_back(pose);
-
-  object_in_map_pub_.publish(cylinder_object);
-
+ 
+ /* nh.deleteParam("posicion_x"); 
+  nh.deleteParam("posicion_y"); 
+  nh.deleteParam("posicion_z"); 
+  nh.deleteParam("rotacion_x"); 
+  nh.deleteParam("rotacion_y"); 
+  nh.deleteParam("rotacion_z"); 
+  nh.deleteParam("rotacion_w"); 
+  nh.deleteParam("width"); 
+  nh.deleteParam("length"); 
+  nh.deleteParam("height"); 
+  nh.deleteParam("name_obstacle");  
+  */
+   
+        ros::Duration(5.0).sleep();
+      object_in_map_pub_.publish(cylinder_object);
   ROS_INFO("Should have published");
-
-  ros::Duration(2.0).sleep();
-
+  
+    ros::Duration(5.0).sleep();
   ros::shutdown();
 
 }
