a2557af231e0198b202a296f06985a0a7ff0c5ab
irojas <irojas@224674b8-e365-4e73-a4a8-558dbbfec58c>
2012-06-19 20:59:49 +0000

    modified move in joints

--- iri_wam_move_arm/src/move_in_joints.cpp
+++ iri_wam_move_arm/src/move_in_joints.cpp
@@ -76,7 +76,6 @@ class MoveInJoints
 		goal.motion_plan_request.num_planning_attempts = 1;
 		goal.motion_plan_request.allowed_planning_time = ros::Duration(5.0);
 		goal.motion_plan_request.planner_id = std::string("");
-		//goal.accept_invalid_goals=true;
 		goal.planner_service_name=std::string("ompl_planning/plan_kinematic_path");
 		goal.motion_plan_request.expected_path_dt = time_move;
 	}
@@ -98,18 +97,6 @@ class MoveInJoints
 	   }
 	  }
 	}
-	//!Defined Path Constraint Parameters
-	void setPlannerRequestPathConstraint(arm_navigation_msgs::MoveArmGoal& goal)
-	{
-	/* goal.motion_plan_request.path_constraints.joint_constraints.resize(names.size());	
-     for(unsigned int i = 0 ; i < names.size(); ++i)
-	 {
-	  goal.motion_plan_request.path_constraints.joint_constraints[i].joint_name = names[i];
-      goal.motion_plan_request.path_constraints.joint_constraints[i].position =0.00; 
-      goal.motion_plan_request.path_constraints.joint_constraints[i].tolerance_below = 10.10001;
-      goal.motion_plan_request.path_constraints.joint_constraints[i].tolerance_above = 10.10001;
-     }*/
-	}	
 };
 int main(int argc, char** argv)
 {
@@ -122,13 +109,11 @@ int main(int argc, char** argv)
   arm.setNamesJoint();
   arm.setPlannerRequest(move);
   arm.setPlannerRequestGoalConstraint(move);
-  arm.setPlannerRequestPathConstraint(move); 
   arm.startTrajectory(move);
   // Wait for trajectory completion
   while(!arm.getState().isDone() && ros::ok())
   {
 	  ROS_DEBUG("WAIT To END TRAJECTORY");
-   // usleep(50000);
   }
       
  bool success = (arm.getState() == actionlib::SimpleClientGoalState::SUCCEEDED);
