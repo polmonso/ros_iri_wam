746d6aa0f5e23cbbe6c572770e267f9151671267
sfoix <sfoix@224674b8-e365-4e73-a4a8-558dbbfec58c>
2013-11-20 14:48:18 +0000

    [iri_wam_controller]  - Adding joints_move missing code

--- iri_wam_controller/include/wam_controller_driver.h
+++ iri_wam_controller/include/wam_controller_driver.h
@@ -188,7 +188,7 @@ class WamControllerDriver : public iri_base_driver::IriBaseDriver
      */
     void move_trajectory_in_joints(const trajectory_msgs::JointTrajectory & trajectory);
     void stop_trajectory_in_joints();
-    void move_in_joints(std::vector<double> *angles, std::vector<double>* vels, std::vector<double>* accs);
+    void move_in_joints(std::vector<double> *angles, std::vector<double>* vels = NULL, std::vector<double>* accs = NULL);
     /**
      * after a /move_trajectory_in_joints
      */
